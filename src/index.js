acf5903412f01bd0ca4a470914c3b4e9fb7950c1
kn8a <mydreamhomefl@gmail.com>
2022-10-03 16:27:54 -0400

    cleanup and minor text fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
@@ -11,7 +10,6 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <StrictMode>
     <ColorModeScript />
-    
     <App />
   </StrictMode>
 );
@@ -24,4 +22,3 @@ serviceWorker.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

fdf22c5f43f9685332da64e7cf4b231b6c10e6b6
kn8a <mydreamhomefl@gmail.com>
2024-01-16 11:48:54 -0500

    Ascii logo

--- src/Components/AsciiArt.js
+++ src/Components/AsciiArt.js
@@ -1,135 +1,115 @@
 import React, { useEffect, useState } from 'react';
 import { Text } from '@chakra-ui/react';
 
-const asciiText = `
-                  ###########                      
-                  ################                 
-                  ###################              
-                 #####################  #          
-              #########################  ##        
-            ############################  ###      
-           ######   #####################  ####    
-          ######   #######################  #####   
-       ###################################  ######  
-     #####################################  ####### 
-  #######################################  ######## 
-########################################  #########
-##  ####################################  ######### 
-########################## ############  ######### 
-############              ############  ########## 
- #########             ##############  ##########  
-   ####              ###############  ###########  
-                  #################  ###########   
-                ##################  ############    
-             ###################  #############     
-           ###################  ##############      
-         ###################  ##############       
-        ##################  ################       
-       ##################  ##################      
-      ##################  ####################     
-      ################  #######################    
-      ###############  ########################    
-       ############## ##########################   
-       ############## ##########################   
-        #######################################     
-          ####################################       
-           ##################################       
-            ################################       
-        ##        ####################        ##    
-    ############                        ########## 
-   ################################################
-   ################################################
-   ################################################
-      ##########################################   
-           ################################        
-  `;
 
-  const searchString = "#"
-        const replacement = "."
-        let regex = new RegExp(searchString, 'g')
-        let replacedString = asciiText.replace(regex, replacement)
+
+
+
+
+const asciiText = [
+
+'                  ###########',
+'                  ################',
+'                  ###################',              
+'                 #####################  #',          
+'              #########################  ##',        
+'            ############################  ###',      
+'           ######   #####################  ####',    
+'          ######   #######################  #####',   
+'       ###################################  ######',  
+'     #####################################  #######', 
+'  ########################################  ########', 
+'#########################################  #########',
+'##  ####################################  ##########', 
+'########################## ############  ##########', 
+'############              ############  ###########', 
+' #########             ##############  ###########',  
+'   ####              ###############  ###########',  
+'                  #################  ###########',   
+'                ##################  ###########',    
+'             ###################  ############',     
+'           ###################  #############',      
+'         ###################  ##############',       
+'        ##################  ################',       
+'       ##################  ##################',      
+'      ##################  ####################',     
+'      ################  #######################',    
+'      ###############  ########################',    
+'       ############## ##########################',   
+'       ############## ##########################',   
+'        #######################################',     
+'          ####################################',       
+'           ##################################',       
+'            ################################',       
+'        ##        ####################        ##',    
+'    ############                        ##########', 
+'   ################################################',
+'   ################################################',
+'   ################################################',
+'      ##########################################',   
+'           ################################',        
+
+]
+
+
+const searchString = "#";
+const replacements = [".", ":", ";", "o", "8", "o"]; // Replace with your desired replacements
 
 const AsciiArt = () => {
-  const [knightText, setKnightText] = useState(replacedString)
+  const asciiLines = asciiText
+
+  const regex = new RegExp(searchString, 'g');
+  const replacedLines = asciiLines.map(line => line.replace(regex, replacements[0]));
+
+  const [knightText, setKnightText] = useState(replacedLines.join('\n'));
   const [offset, setOffset] = useState(0);
+
+
+
   useEffect(() => {
     const onScroll = () => {
       setOffset(window.scrollY);
-      if (window.scrollY < 135) {
-        const searchString = "#"
-        const replacement = "."
-        let regex = new RegExp(searchString, 'g')
-        let replacedString = asciiText.replace(regex, replacement)
-        setKnightText(replacedString)
-        
-    } 
-    else if (window.scrollY > 134 && window.scrollY < 250) {
-      const searchString = "#"
-        const replacement = ":"
-        let regex = new RegExp(searchString, 'g')
-        let replacedString = asciiText.replace(regex, replacement)
-        setKnightText(replacedString)
-        
-    } else if (window.scrollY > 249 && window.scrollY < 320) {
-      const searchString = "#"
-        const replacement = ";"
-        let regex = new RegExp(searchString, 'g')
-        let replacedString = asciiText.replace(regex, replacement)
-        setKnightText(replacedString)
-        
-    }
-    else if (window.scrollY > 319 && window.scrollY < 450) {
-      const searchString = "#"
-        const replacement = "o"
-        let regex = new RegExp(searchString, 'g')
-        let replacedString = asciiText.replace(regex, replacement)
-        setKnightText(replacedString)
-        
-    } else if (window.scrollY > 449 && window.scrollY < 600) {
-      const searchString = "#"
-        const replacement = "8"
-        let regex = new RegExp(searchString, 'g')
-        let replacedString = asciiText.replace(regex, replacement)
-        setKnightText(replacedString)
-        
-    } else if (window.scrollY > 599 && window.scrollY < 1000) {
-      const searchString = "#"
-        const replacement = "o"
-        let regex = new RegExp(searchString, 'g')
-        let replacedString = asciiText.replace(regex, replacement)
-        setKnightText(replacedString)
-        
-    }
 
-    }
-    // clean up code
+      let replacementIndex = 0;
+      
+
+      if (window.scrollY > 134 && window.scrollY < 250) {
+        replacementIndex = 1;
+      } else if (window.scrollY > 249 && window.scrollY < 320) {
+        replacementIndex = 2;
+      } else if (window.scrollY > 319 && window.scrollY < 450) {
+        replacementIndex = 3;
+      } else if (window.scrollY > 449 && window.scrollY < 600) {
+        replacementIndex = 4;
+      } else if (window.scrollY > 599 && window.scrollY < 700) {
+        replacementIndex = 3;
+      } else if (window.scrollY > 699 && window.scrollY < 800) {
+        replacementIndex = 2;
+      } else if (window.scrollY > 799 && window.scrollY < 900) {
+        replacementIndex = 2;
+      }
+      const regex = new RegExp(searchString, 'g');
+      const replacedLines = asciiLines.map(line => line.replace(regex, replacements[replacementIndex]));
+      setKnightText(replacedLines.join('\n'));
+    };
+
     window.removeEventListener('scroll', onScroll);
     window.addEventListener('scroll', onScroll, { passive: true });
+
     return () => window.removeEventListener('scroll', onScroll);
   }, []);
-  
 
-  console.log(offset);
+
   const asciiArtStyle = {
     lineHeight: '1', // Adjust the value to reduce or increase line spacing
-  
   };
 
-  
-  
-
-  const [text,setText] = useState(asciiText)
-  
-
   return (
-    <Text textColor={'gray'} justifyContent={'center'} justifyItems={'center'}  fontSize={11}>
-        <pre style={asciiArtStyle}>
-        
-      {knightText}
-    
-    </pre>
+    <Text textColor={'gray'} justifyContent={'center'} justifyItems={'center'} fontSize={11}>
+      <pre style={asciiArtStyle}>
+        {knightText}
+      </pre>
     </Text>
-    
   );
 };
 
